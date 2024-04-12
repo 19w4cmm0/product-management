a6f491193fcde2f3ddcac0935c048417efcd2d20
19w4cmm0 <1050080129@sv.hcmunre.edu.vn>
2024-04-12 19:17:51 +0700

    ALL

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,37 @@
-module.exports.product = (req, res) => {
-    res.render("client/pages/home/product/index")
+const Product = require("../../models/product.model");
+// [GET] /products
+module.exports.product = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    }).sort({position: "desc"});
+
+    const newProduct = products.map(item => {
+        item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
+        return item;
+    });
+
+
+    res.render("client/pages/product/index", {
+        pageTitle: "Danh sách sản phẩm",
+        products: products
+});
+}
+
+// [GET] /product/:slug
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            status: "active",
+            slug: req.params.slug
+        }
+        const product = await Product.findOne(find);
+        res.render("client/pages/product/detail", {
+            pageTitle: product.title,
+            product: product
+    });
+    } catch(error) {
+        res.redirect(`/product`);
+    }
 }
--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express')
+const flash = require('express-flash')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser');
 const route = require('./routes/client/index.route')
+const routeAdmin = require('./routes/admin/index.route')
 const app = express()
+const mongoose = require("mongoose");
+const database = require("./config/database");
+const systemConfig = require("./config/system");
+
 require('dotenv').config()
+
+database.connect();
+
+mongoose.connect(process.env.MONGO_URL);
+
 const port = process.env.PORT;
 
-app.set('views', './views')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(methodOverride('_method'));
+
+app.set('views', `${__dirname}/views`)
 app.set('view engine', 'pug')
 
+// Flash
+
+app.use(cookieParser('XXXXXXXXXX'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+
+// End Flash
+
+// App locals Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+app.use(express.static(`${__dirname}/public`));
+
   route(app);
+  routeAdmin(app);
   
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
